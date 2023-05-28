adcd65c1262e9615be120a85461ffc8d7cf408bb
AlekAz <vakarin2020@gmail.com>
2023-05-29 01:38:28 +0300

    Миграции

---
+++ src/EazyQuiz.Web.Api/Migrations/20230528223803_ThemesAddFeedback.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EazyQuiz.Web.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThemesAddFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Themes_ThemeId",
+                table: "Questions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Themes",
+                table: "Themes");
+
+            migrationBuilder.RenameTable(
+                name: "Themes",
+                newName: "Theme");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Theme",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Theme",
+                table: "Theme",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Feedbacks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedbacks_UserId",
+                table: "Feedbacks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Theme_ThemeId",
+                table: "Questions",
+                column: "ThemeId",
+                principalTable: "Theme",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Theme_ThemeId",
+                table: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Theme",
+                table: "Theme");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Theme");
+
+            migrationBuilder.RenameTable(
+                name: "Theme",
+                newName: "Themes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Themes",
+                table: "Themes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Themes_ThemeId",
+                table: "Questions",
+                column: "ThemeId",
+                principalTable: "Themes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
